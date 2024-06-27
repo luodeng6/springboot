961c9ddc6570929bec73c376af0e390ec759df81
罗邓 <12926664+luo-deng6@user.noreply.gitee.com>
2024-06-27 09:54:53 +0800

    添加了一个项目，新增excel读取的两个方法

---
+++ Many_algorithms/src/main/java/org/deng/many_algorithms/Service/ExcelService.java
@@ -0,0 +1,31 @@
+package org.deng.many_algorithms.Service;
+
+import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+
+public interface ExcelService {
+    /**
+     * 读取Excel文件中的数据
+     *
+     * @param filePath Excel文件的路径
+     * @return 包含Excel文件数据的列表，每个子列表代表一行数据
+     * @throws IOException 如果文件读取过程中发生I/O错误
+     */
+    List<List<String>>  readExcelList(String filePath) throws IOException;
+
+
+    /**
+     * 读取Excel文件中的数据，并将数据转换为Map, 其中key为Excel文件第一行的标题
+     *
+     * @param filePath Excel文件的路径
+     * @return 包含Excel文件数据的Map列表，每个Map代表一行数据
+     * @throws IOException 如果文件读取过程中发生I/O错误
+     */
+    List<Map<String, String>> readExcelMap(String filePath) throws IOException;
+
+}
